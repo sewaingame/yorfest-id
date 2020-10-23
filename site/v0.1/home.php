e74b114c285fd81c537d820ad4e89c7a08db2f27
mardikareza <mardika.reza@gmail.com>
2020-10-24 05:03:27 +0700

    Revert "Update"
    This reverts commit b8490bf7b198ae6e3b410018bab8dd5aab6b6016.

---
+++ site/v0.1/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <a href="networking/">Netorking</a>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap jQuery -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Counter -->
+    <script src="js/jquery.appear.min.js"></script>
+    <!-- Countdown -->
+    <script src="js/jquery.jCounter.js"></script>
+    <!-- magnific-popup -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!-- carousel -->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Waypoints -->
+    <script src="js/wow.min.js"></script>
+    <!-- isotop -->
+    <script src="js/isotope.pkgd.min.js"></script>
+    <!-- Template custom -->
+    <script src="js/main.js"></script>
+    <!-- App -->
+    <script src="app/keycheck.js"></script>
+  </body>
+</html>
