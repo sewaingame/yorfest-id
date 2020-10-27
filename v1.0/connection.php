aefbb209e97e696fde77e2c83be87d9eb20b6954
mardikareza <mardika.reza@gmail.com>
2020-10-28 00:36:12 +0700

    Update connection.php

--- v1.0/connection.php
+++ v1.0/connection.php
@@ -1,6 +1,6 @@
 <?php
- // $con = mysqli_connect("localhost","u1132840_yorfest2020","chelsea2018","u1132840_yorfest2020");
-  $con = mysqli_connect("localhost","root","","yorfest2020");
+ $con = mysqli_connect("localhost","u1132840_yorfest2020","chelsea2018","u1132840_yorfest2020");
+  // $con = mysqli_connect("localhost","root","","yorfest2020");
 //
 // Check connection
 if (mysqli_connect_errno()) {
