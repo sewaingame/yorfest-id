220c1253b2e0af3dc93a5e317780fbd2a0d3629d
mardikareza <mardika.reza@gmail.com>
2020-10-28 01:02:37 +0700

    Update conference.php

--- v1.0/conference.php
+++ v1.0/conference.php
@@ -774,6 +774,7 @@
       <script src="js/isotope.pkgd.min.js"></script>
       <!-- Template custom -->
       <script src="js/main.js"></script>
+      <script src="js/checknotification.js"></script>
       </div>
       <!-- Body inner end -->
    </body>
