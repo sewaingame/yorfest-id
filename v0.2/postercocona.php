5378900799555505e58c0dfe2e57b45ed5ad37ff
thesourich <72848496+thesourich@users.noreply.github.com>
2020-10-27 19:07:52 +0700

    Update Link Catalog and Poster
    Ceploook

---
+++ v0.2/postercocona.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs ================================================== -->
+   <meta charset="utf-8">
+
+   <!-- Mobile Specific Metas ================================================== -->
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+   <meta name ="description" content="Young organic festival 2020">
+   <meta name ="og;description" content="Young organic festival 2020">
+   <meta name ="og;url" content="https;//yorfest.id/">
+
+   <!-- Site Title -->
+   <title>YORFEST - 2020 &amp; YOUNG ORGANIC FESTIVAL 2020 </title>
+   <link rel="shortcut icon" type="image/x-icon" href="images/logo_tab.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+  </head>
+  <style media="screen">
+  #bt1 {
+    position: absolute;
+    left: 10px;
+    top: 10px;
+  }
+  body {
+  margin: 0;
+  }
+  iframe {
+    height:calc(100vh - 4px);
+    width:calc(100vw - 4px);
+    box-sizing: border-box;
+  }
+  </style>
+  <body>
+
+    <iframe src="https://drive.google.com/file/d/1WAphjIsq6buQEGbpzbY4WUHIHEKYlWXh/preview" width="100%" height="100%"></iframe>
+
+    <a id="bt1" class="btn btn-primary" href="https://drive.google.com/file/d/1WAphjIsq6buQEGbpzbY4WUHIHEKYlWXh/view?usp=sharing" target="_blank" role="button">Download</a>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
